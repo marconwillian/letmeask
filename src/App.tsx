f13754672b28898488bdd1ef009b5dd3266bc445
Marcon Willian <oi@marconwillian.dev>
2021-06-20 12:46:13 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+      <>
+        <h1>Hello World</h1>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </>
+
+  );
+}
+
+export default App;
