8408c200f95fd69e2483182c14a77884ef5d0aed
Marcon Willian <oi@marconwillian.dev>
2021-06-21 23:00:16 -0300

    chore ✨️: Config base of firebase auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import logoImg from '../assets/images/logo.svg';
 import googleImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
+import { auth, firebase } from '../services/firebase';
 
 import '../styles/auth.scss';
 
@@ -12,6 +13,15 @@ import '../styles/auth.scss';
 export function Home() {
     const history = useHistory();
 
+
+    function handleCreateRoom(){
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+
+            history.push('/rooms/new');
+        })
+    }
     return (
         <div id="page-auth">
             <aside>
@@ -22,7 +32,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="LetMeAsk" />
-                    <button className="create-room" >
+                    <button className="create-room" onClick={() => handleCreateRoom()}>
                         <img src={googleImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
