014e7b2522df6fc7e22dc58445bc96aac0ed5a3c
Marcon Willian <oi@marconwillian.dev>
2021-06-22 23:14:06 -0300

    chore ✨️: Create base of room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,34 @@
+
+import '../styles/room.scss'
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+
 export function Room() {
     return (
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>codigo</div>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form >
+                    <textarea 
+                        placeholder="O que você quer perguntar?"
+                    />
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu Login</button>.</span>
+                        <Button type="submit">Enviar Pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
