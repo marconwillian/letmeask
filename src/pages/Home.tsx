a47f818bbf4a6400d066956d4afe0d6e5fbd4978
Marcon Willian <oi@marconwillian.dev>
2021-06-22 22:42:09 -0300

    chore ✨️: Config base of pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,22 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import { database } from '../services/firebase';
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleImg from '../assets/images/google-icon.svg';
-
-import { Button } from '../components/Button';
-
 import '../styles/auth.scss';
-import { useAuth } from '../hooks/useAuth';
+
+
 
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [ roomCode, setRoomCode ] = useState('');
 
 
     async function handleCreateRoom(){
@@ -20,6 +25,24 @@ export function Home() {
         }
 
         history.push('/rooms/new');
+    }
+
+    
+    async function handleJoinRoom(event: FormEvent){
+        event.preventDefault();
+
+        if(roomCode.trim() === ''){
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()){
+            alert('Room does not exists.')
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
     return (
         <div id="page-auth">
@@ -36,10 +59,12 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text" 
                             placeholder="Digite o código da sala"
+                            value={roomCode}
+                            onChange={event => setRoomCode(event.target.value)}
                         />
                         <Button type="submit">
                             Entrar na sala
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,39 @@
-import {Link} from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+
+import {Link, useHistory} from 'react-router-dom';
+
+import { database } from "../services/firebase";
+import { useAuth } from '../hooks/useAuth';
+import { Button } from '../components/Button';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
-import { useAuth } from '../hooks/useAuth';
-import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 
 
-export function NewRoom() {
-    const { user, signInWithGoogle } = useAuth();
+export  function NewRoom() {
+    const history = useHistory();
+    const { user } = useAuth();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent){
+        event.preventDefault();
+
+        if(newRoom.trim() === ''){
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
 
     return (
         <div id="page-auth">
@@ -23,10 +46,12 @@ export function NewRoom() {
                 <div className="main-content">
                     <img src={logoImg} alt="LetMeAsk" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input 
                             type="text" 
                             placeholder="Nome da sala"
+                            value={newRoom}
+                            onChange={event => setNewRoom(event.target.value)}
                         />
                         <Button type="submit">
                             Criar na sala
